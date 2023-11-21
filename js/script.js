3b8ac2e45cc8fa7314237ce04ca24aa3af535808
Mustafa-Sayed-M <mustafaelsadiq9@gmail.com>
2023-11-21 08:01:51 +0200

    Create a Features Section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-// Get Elements:
+// ## Get Elements:
+// # Header:
 const menu = document.querySelector('header nav .links')
-// Function to toggle menu
+// --------------------------
+// # Features:
+const listItems = document.querySelectorAll('.features .list ul li');
+const listSlides = document.querySelectorAll('.features .slides .slide');
+// --------------------------
+// ## Functions:
+// # Function to toggle menu
 function toggleMenu() {
     menu.classList.toggle('active');
+}
+// # Function to add active class when user click on this element
+function addActiveClass(ele) {
+    removeActiveClassFromItems(listItems)
+    ele.classList.add('active');
+}
+// # Function to remove active class from all items
+function removeActiveClassFromItems(arr) {
+    arr.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+// # Function to change slide
+function changeSlide(item) {
+    const targetIndex = item.dataset.index;
+    removeActiveClassFromItems(listSlides);
+    listSlides[targetIndex].classList.add('active');
 }
